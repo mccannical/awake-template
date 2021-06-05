4c5c9a2dad07ed09a385e6ead7475bf9b944b4b2
Jesse <jesse@mccannical.com>
2021-06-05 13:06:07 -0400

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -11,7 +11,7 @@ import theCMS from '../cms/netlify/cms'
 export default {
   siteName: 'Basement Garden',
   tagline: 'Gardening adventures in my basement',
-  featureImage: 'plants.jpg',
+  featureImage: '/uploads/plants.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
